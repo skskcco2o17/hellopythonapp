f0a499c571872b252eeae71363f3c30f126d649b
skskcco2o17 <34002271+skskcco2o17@users.noreply.github.com>
2020-02-04 11:27:19 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -18,7 +18,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-    return "Hello supal this My first Hello Python World!\r\n", 200, { 'Content-Type': 'text/plain' }
+    return "Hello Anand, this My first Hello Python World!\r\n", 200, { 'Content-Type': 'text/plain' }
 
 if __name__ == '__main__':
     application.run(debug = True)
